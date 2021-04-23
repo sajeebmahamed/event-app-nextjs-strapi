a32f77f08b70c6d203658aa5c41e484d9dde173c
sajeeb-staffasia <sajeebahamed@staffasia.org>
2021-04-23 23:06:30 +0600

    404 page added

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p> Copyright &copy; Events 2021 </p>
+      <p>
+        <Link href="/about"> About this project </Link>
+      </p>
+    </footer>
+  );
+}
