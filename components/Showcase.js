a1671413d67958603aa4cd391aa13e4be4ddb847
sajeeb-staffasia <sajeebahamed@staffasia.org>
2021-04-23 23:27:29 +0600

    banner added

---
+++ components/Showcase.js
@@ -0,0 +1,8 @@
+import styles from "../styles/Showcase.module.css";
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1> Welcome To the party </h1>
+    </div>
+  );
+}
